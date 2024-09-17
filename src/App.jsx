27113d4f48db68002f0dc26a2d1750050dea743f
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-09-17 05:40:05 +0000

    basic-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-function App() {
-  // const apiUrl = import.meta.env.VITE_API_URL;
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux";
+import authService from './appwrite/auth';
+import {login, logout} from "./store/authSlice"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
+import { Outlet } from "react-router-dom";
 
-  return (
-    <>
-      <h1>
-        React-Js in Vite
-      </h1>
-    </>
-  )
+function App() {
+  const [loading,setLoading]=useState(true);
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{console.error("ERROR- ",error)})
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading?(
+  <>
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  </>
+  ):(null)
 }
 
 export default App
