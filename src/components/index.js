04b76d413873fa4c0d4fac8ffacbd8830d1a3c6a
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-09-18 10:15:52 +0000

    Login,AuthRoute

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container';
 import Logo from './Logo'; 
 import LogoutButton from "./Header/LogoutButton";
+import Input from './Input';
+import Button from './Button';
 
-export {Header,Footer,Container,Logo,LogoutButton}
+export {Header,Footer,Container,Logo,LogoutButton,Input,Button}
