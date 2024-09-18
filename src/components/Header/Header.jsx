1b937125731f7f753b52ee96df6b0a1098ad6001
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-09-18 05:55:45 +0000

    issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Container,Logo,LogoutButton} from '../index'
-import { Link,useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Container, Logo, LogoutButton}  from '../index'
+import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function Header  () {
+function Header() {
   const authStatus = useSelector((state) => state.auth.status)
-  const navigate=useNavigate();
+  const navigate = useNavigate()
+
   const navItems = [
     {
       name: 'Home',
@@ -34,36 +36,37 @@ function Header  () {
   },
   ]
 
+
   return (
     <header className='py-3 shadow bg-gray-500'>
-    <Container>
-      <nav className='flex'>
-        <div className='mr-4'>
-          <Link to='/'>
-            <Logo width='70px'   />
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px'   />
 
-            </Link>
-        </div>
-        <ul className='flex ml-auto'>
-          {navItems.map((item) => 
-          item.active ? (
-            <li key={item.name}>
-              <button
-              onClick={() => navigate(item.slug)}
-              className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-              >{item.name}</button>
-            </li>
-          ) : null
-          )}
-          {authStatus && (
-            <li>
-              <LogoutBtn />
-            </li>
-          )}
-        </ul>
-      </nav>
-      </Container>
-  </header>
+              </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutButton />
+              </li>
+            )}
+          </ul>
+        </nav>
+        </Container>
+    </header>
   )
 }
 
