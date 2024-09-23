2c17413d49c39c96f4cf5e5ef4933fd983efa98f
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-09-23 10:19:17 +0000

    style-logout

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -16,7 +16,7 @@ function LogoutButton() {
         })
     }
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={logoutHandler} >
+    <button className='d-inline-block custom-px-6 py-2 rounded-pill custom-hover-bg-blue' onClick={logoutHandler} >
         Logout
     </button>
   )
